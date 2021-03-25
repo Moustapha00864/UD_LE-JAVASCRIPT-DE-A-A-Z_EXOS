b98793e4207c70234708c9953edc86b08647b0f4
jcharles b <jcharles.b@icloud.com>
2021-03-25 09:29:12 +0100

    Exo 1 - Les types et les variables

---
+++ EXO 1/script.js
@@ -0,0 +1,36 @@
+//------------ EXERCICE 1 : Les variables, commentaires & types  ------------//
+
+
+// Écrire un commentaire sur plusieurs ligne
+/*
+ Voici un commentaire
+ sur
+ plusieurs lignes
+ */
+
+
+// Déclarez une constante "data" qui contient le nombre 3
+const data = 3;
+
+
+// Écrivez des données correspondant à ces différents types
+
+// String (texte)
+let str = "Une chaîne de caractères";
+
+// Number (nombres)
+let nb = 5;
+
+// Object (object)
+let myObj = {
+	prenom: "jcharles",
+	nom   : "b",
+	age   : 52
+};
+
+// Boolean (booléen)
+let bool = true;
+
+
+// Déclarez trois variables sans valeur(undefined) en une ligne.
+let prenom, nom, age;
